16463fd14d9add4e3b5c2de5d4d8b6c11a96f24e
mowenpark <michaelowenpark@gmail.com>
2016-02-23 10:00:17 -0800

    add session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,24 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+
+    if user
+      sign_in(user)
+      flash[:errors] = "Welcome back!"
+      redirect_to user_url(user)
+    else
+      flash.now[:errors] = "Credentials were incorrect, please try again."
+      render :new
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to new_session_url
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,33 @@
 class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in(@user)
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
+  def edit
+    @user = User.find(params[:id])
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
 end
