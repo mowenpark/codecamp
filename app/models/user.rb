5931b919cc137e5b90412620e00164c8c575dc99
mowenpark <michaelowenpark@gmail.com>
2016-02-23 09:43:28 -0800

    add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,41 @@
 class User < ActiveRecord::Base
+  attr_reader :password
+
+  validates :email, :session_token, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :password_digest, presence: { message: "Password can't be blank" }
+
+  after_initialize :ensure_session_token
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def valid_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = self.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by_email(email)
+    return nil unless user && user.valid_password?(password)
+    user
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= self.generate_session_token
+  end
+
 end
