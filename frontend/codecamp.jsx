275a227b37139b904d5707ba0341a04b9a448f37
Michael Park <michaelowenpark@gmail.com>
2020-03-27 01:36:50 -0700

    Chore/update webpack (#15)
    * chore(packages): add package-lock.json
    
    * fix(spring): fix gemfile warning
    
    * fix(application): comment out active record
    config for raise callbacks
    
    * fix(Postgresql): add db username and pass, update seed
    file for broken logo images
    
    * fix(bundle): update bundle files
    
    * fix(webpack): update deps, configure webpack

--- frontend/codecamp.jsx
+++ frontend/codecamp.jsx
@@ -1,49 +1,47 @@
-var ReactDOM = require('react-dom'),
-    React = require('react'),
-    NavBarMain = require('./components/navbar_main'),
-    Tabs = require('./components/tabs'),
-    User = require('./components/user'),
-    SignIn = require('./components/signin'),
-    SignUp = require('./components/signup'),
-    Companies = require('./components/companies'),
-    Dashboard = require('./components/dashboard'),
-    Company = require('./components/company'),
-    Footer = require('./components/footer');
+import "babel-polyfill";
+var ReactDOM = require("react-dom"),
+  React = require("react"),
+  NavBarMain = require("./components/navbar_main"),
+  Tabs = require("./components/tabs"),
+  User = require("./components/user"),
+  SignIn = require("./components/signin"),
+  SignUp = require("./components/signup"),
+  Companies = require("./components/companies"),
+  Dashboard = require("./components/dashboard"),
+  Company = require("./components/company"),
+  Footer = require("./components/footer");
 
-var Router = require('react-router').Router;
-var IndexRoute = require('react-router').IndexRoute;
-var Route = require('react-router').Route;
-var Link = require('react-router').Link;
+var Router = require("react-router").Router;
+var IndexRoute = require("react-router").IndexRoute;
+var Route = require("react-router").Route;
+var Link = require("react-router").Link;
 
 var App = React.createClass({
-
-  render: function(){
+  render: function() {
     return (
-        <div>
-          <NavBarMain />
-          <div className="bod">
-            {this.props.children}
-          </div>
-          <Footer />
-        </div>
+      <div>
+        <NavBarMain />
+        <div className="bod">{this.props.children}</div>
+        <Footer />
+      </div>
     );
   }
 });
 
 var routes = (
-    <Route path="/" component={App}>
-      <IndexRoute component={Dashboard}/>
-      <Route path="users/:id" component={User} />
-      <Route path="programs" component={Tabs} />
-      <Route path="companies" component={Companies} />
-      <Route path="languages" component={Tabs} />
-      <Route path="/companies/:id" component={Company}/>
-      <Route path="signin" component={SignIn} />
-      <Route path="signup" component={SignUp} />
-    </Route>
+  <Route path="/" component={App}>
+    <IndexRoute component={Dashboard} />
+    <Route path="users/:id" component={User} />
+    <Route path="programs" component={Tabs} />
+    <Route path="companies" component={Companies} />
+    <Route path="languages" component={Tabs} />
+    <Route path="/companies/:id" component={Company} />
+    <Route path="signin" component={SignIn} />
+    <Route path="signup" component={SignUp} />
+  </Route>
 );
 
-document.addEventListener("DOMContentLoaded", function () {
-  var content = document.getElementById('content');
+document.addEventListener("DOMContentLoaded", function() {
+  var content = document.getElementById("content");
   ReactDOM.render(<Router>{routes}</Router>, content);
 });
