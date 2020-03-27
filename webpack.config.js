f5e971f14df1e47b6e25ac69ebaeb8392bfd5011
Michael Park <michael.park@mirumagency.com>
2020-03-27 01:32:58 -0700

    fix(webpack): update deps, configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,25 @@
 var path = require("path");
 
 module.exports = {
-  context: __dirname,
   entry: "./frontend/codecamp.jsx",
   output: {
-    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
     filename: "bundle.js"
   },
   resolve: {
-    extensions: ["", ".js", ".jsx"]
+    extensions: [".js", ".jsx", ".json"]
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel',
-        query: {
-          presets: ['react']
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
         }
-      },
-      {
-        test: /\.node$/,
-        loader: "node-loader"
       }
     ]
   },
