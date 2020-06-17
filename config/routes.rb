8bf6ddbaa6ec885c7b88b4d76d0b25ec879cb49c
Michael Park <michael.park@mirumagency.com>
2020-06-16 23:10:00 -0700

    update rails app

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,3 @@
 Rails.application.routes.draw do
-
-  root to: "static_pages#index"
-
-  namespace :api, defaults: {format: :json} do
-    resources :programs
-    resources :reviews
-    resources :companies
-    resource :session
-    resources :users
-    resources :languages
-    resources :follows
-  end
-
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
