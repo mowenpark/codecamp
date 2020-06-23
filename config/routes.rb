26ff5ac078ca0f561954594a01fbf42ed349cb65
Michael Park <michael.park@mirumagency.com>
2020-06-23 16:16:17 -0700

    fix/add back rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "static_pages#index"
+
+  namespace :api, defaults: {format: :json} do
+    resources :programs
+    resources :reviews
+    resources :companies
+    resource :session
+    resources :users
+    resources :languages
+    resources :follows
+  end
 end
